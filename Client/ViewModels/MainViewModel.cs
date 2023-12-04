28b4468541c09a93dbe9d381e5f1d103cac25da7
hasanhttps <hasanabdullazad@gmail.com>
2023-12-04 23:16:17 +0400

    Latest Version 1.0

---
+++ Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Client.Commands;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Windows.Media;
+using System.Collections.Generic;
+
+namespace Client.ViewModels;
+
+public class MainViewModel : INotifyPropertyChanged {
+
+    // Private Fields
+
+    private string fileName;
+    private BitmapImage _imageData;
+
+    // Binding Properties
+
+    public BitmapImage? ImageData { get => _imageData;
+        set {
+            _imageData = value;
+            OnProperty();
+        }
+    }
+    public ICommand? ScreenshotButtonCommand { get; set; }
+
+    // Constructor
+
+    public MainViewModel() {
+
+        SetCommands();
+        ListenClientAsync();
+    }
+
+    // Functions
+
+    private void SetCommands() {
+
+        ScreenshotButtonCommand = new RelayCommand(TakeScreenshot);
+    }
+
+    private void TakeScreenshot(object? param) {
+
+        var client = new UdpClient();
+
+        var Ip = IPAddress.Parse("127.0.0.1");
+        var Port = 27002;
+
+        var remoteEP = new IPEndPoint(Ip, Port);
+
+        var msg = "takescreenshot";
+        var len = 0;
+        var buffer = Array.Empty<byte>();
+
+        buffer = Encoding.Default.GetBytes(msg);
+        client.SendAsync(buffer, remoteEP);
+    }
+
+    private async Task ListenClientAsync() {
+
+        UdpClient server = new UdpClient(27003);
+        var buffer = new byte[ushort.MaxValue - 29];
+        var list = new List<byte>();
+
+        while (true) {
+            while (true) {
+                var result = await server.ReceiveAsync();
+                buffer = result.Buffer;
+                var len = buffer.Length;
+                if (len != ushort.MaxValue - 29) break;
+                list.AddRange(buffer);
+            }
+            list.AddRange(buffer);
+            ImageData = LoadImage(list.ToArray());
+            list.Clear();
+        }
+    }
+
+    public static BitmapImage LoadImage(byte[] imageData) {
+        if (imageData == null || imageData.Length == 0) return null;
+
+        var image = new BitmapImage();
+
+        using (var mem = new MemoryStream(imageData)) {
+            mem.Position = 0;
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = null;
+            image.StreamSource = mem;
+            image.EndInit();
+        }
+        image.Freeze();
+        return image;
+    }
+
+    // INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnProperty([CallerMemberName] string? name = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+
+Bitmap? imageData = null;
+
+void TakeScreenShot() {
+    try {
+        Console.WriteLine("Process started ...");
+
+        Bitmap memoryImage = new Bitmap(1920, 1080);
+        Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+        Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+        memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+        imageData = memoryImage;
+    }
+    catch (Exception ex) {
+        Console.WriteLine(ex);
+    }
+}
+
+static byte[] ConvertBitmapToByteArray(Bitmap bitmap) {
+    using (MemoryStream stream = new MemoryStream()) {
+
+        bitmap.Save(stream, ImageFormat.Jpeg);
+
+        return stream.ToArray();
+    }
+}
+
+
+var listener = new UdpClient(27002);
+var client = new UdpClient(27001);
+
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+var msg = "";
+var len = 0;
+
+var connectEP = new IPEndPoint(IPAddress.Parse("127.0.0.2"), 27003);
+
+while (true) {
+
+    var buffer = listener.Receive(ref remoteEP);
+
+    await Task.Run(async () => {
+
+        msg = Encoding.Default.GetString(buffer);
+
+        if (msg == "takescreenshot") {
+
+            while (true) {
+
+                TakeScreenShot();
+                byte[] image = ConvertBitmapToByteArray(imageData);
+                var chunks = image.Chunk(ushort.MaxValue - 29);
+
+                foreach (var item in chunks) {
+                    await client.SendAsync(item, item.Length, connectEP);
+                }
+            }
+        }
+    });
+}
